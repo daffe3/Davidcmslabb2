2e879d2a0f04abdd9d560ef808c6de94b9a58aa2
david-arvidsson <david.arvidsson@humly.io>
2025-04-06 14:07:44 +0200

    updated css + little contentful

--- app/page.js
+++ app/page.js
@@ -5,17 +5,21 @@ export default async function HomePage() {
   const homepage = await getHomepageData();
 
   return (
-    <main className="min-h-screen p-10 bg-gray-100">
-      <h1 className="text-4xl font-bold">{homepage?.title || "Välkommen!"}</h1>
-      <p>{homepage?.presentationText || "Ingen text hittad."}</p>
+    <div className="min-h-screen bg-beige text-orangeAccent p-10">
+      <main>
+        <h1 className="text-4xl font-bold">
+          {homepage?.title || "Välkommen!"}
+        </h1>
+        <p>{homepage?.presentationText || "Ingen text hittad."}</p>
 
-      {homepage?.image?.url && (
-        <img
-          src={homepage.image.url}
-          alt={homepage.image.description || "Bild"}
-          className="mt-4 rounded shadow"
-        />
-      )}
-    </main>
+        {homepage?.image?.url && (
+          <img
+            src={homepage.image.url}
+            alt={homepage.image.description || "Bild"}
+            className="mt-4 rounded shadow"
+          />
+        )}
+      </main>
+    </div>
   );
 }
